a011a87f84b0824bca322903923fd3f0e7f88a68
kgiesbers <keliangiesbers@msn.com>
2025-04-04 13:42:47 +0200

    Removed the creating of classes and objects SQLalchemy does this already, it being an ORM.

--- data_access/database/database_interactors/database_helpers/populate_db.py
+++ data_access/database/database_interactors/database_helpers/populate_db.py
@@ -11,57 +11,62 @@ def populate_database(data):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for competition_data in data:
+    for competition_name, competition_data in data.items():
 
         # Check if competition exists, otherwise create it
         competition = session.query(Competition).filter_by(
-            name=competition_data.name
+            name=competition_name
         ).first()
 
         if not competition:
             competition = Competition(
-                name=competition_data.name,
-                url=competition_data.url
+                name=competition_name,
+                url=competition_data["url"]
             )
-            session.add(competition)
-            session.flush()
+        session.add(competition)
 
-        for bracket_data in competition_data.brackets:
+        for bracket_name, bracket_data in competition_data["brackets"].items():
 
-            # Check if bracket exists, otherwise create it
-            bracket = session.query(Bracket).filter_by(
-                name=bracket_data.name,
-                competition_id=competition.id
-            ).first()
+            # Check if listing is valid BEFORE adding bracket
+            if isinstance(bracket_data["listing"], dict):
 
-            if not bracket:
-                bracket = Bracket(
-                    name=bracket_data.name,
-                    url=bracket_data.url,
+                # Check if bracket exists, otherwise create it
+                bracket = session.query(Bracket).filter_by(
+                    name=bracket_name,
                     competition_id=competition.id
-                )
-                session.add(bracket)
-                session.flush()
+                ).first()
 
-            for listing_data in bracket_data.listing:
+                if not bracket:
+                    bracket = Bracket(
+                        name=bracket_name,
+                        url=bracket_data["url"],
+                        competition_id=competition.id
+                    )
+                session.add(bracket)
+
+            # If listing is not valid, continue to next
+            else:
+                continue
+
+            for listing_place, listing_data in bracket_data["listing"].items():
 
                 # Check if listing exists, otherwise create it
                 listing = session.query(Listing).filter_by(
-                    place=listing_data.place,
-                    number=listing_data.number,
-                    couple=listing_data.couple,
+                    place=listing_place,
+                    number=listing_data["number"],
+                    couple=listing_data["couple"],
                     bracket_id=bracket.id
                 ).first()
 
                 if not listing:
                     listing = Listing(
-                        place=listing_data.place,
-                        number=listing_data.number,
-                        couple=listing_data.couple,
+                        place=listing_place,
+                        number=listing_data["number"],
+                        couple=listing_data["couple"],
                         bracket_id=bracket.id
                     )
-                    session.add(listing)
-                    session.flush()
+                session.add(listing)
+                session.flush()
 
     session.commit()
     session.close()
