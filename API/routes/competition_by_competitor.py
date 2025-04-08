8ccb616c120bd42a3e465d5cbbf3ca59023af3f3
kgiesbers <keliangiesbers@msn.com>
2025-04-08 19:56:28 +0200

    Added first endpoint (competition_by_competitor)

---
+++ API/routes/competition_by_competitor.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from data_access.database.database_interactors.database_queries.competition_by_competitor import competition_by_competitor
+
+
+router = APIRouter()
+
+
+@router.get("/competition_by_competitor/{competitor_name}")
+def read_competition_by_competitor(competitor_name: str):
+    return competition_by_competitor(competitor_name)
+
+
+
