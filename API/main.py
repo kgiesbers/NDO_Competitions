15759efffba6199c3313cdd72f969c930caa6194
kgiesbers <keliangiesbers@msn.com>
2025-12-03 14:08:42 +0100

    Added middleware so API can communicate with frontend

--- API/main.py
+++ API/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from API.routes.competition_by_competitor import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_methods=["GET"],
+    allow_headers=["*"],  # allow all headers
+)
